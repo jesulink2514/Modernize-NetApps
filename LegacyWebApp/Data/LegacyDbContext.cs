ecf6ec5fae403e2b620bee6a6b16d62e932eb6e5
Jesus Angulo <jangulo@baufest.com>
2022-06-22 17:40:16 -0500

    Adding CRUD

---
+++ LegacyWebApp/Data/LegacyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LegacyWebApp.Models;
+using System.Data.Entity;
+
+namespace LegacyWebApp.Data
+{
+    public class LegacyDbContext: DbContext
+    {
+        public LegacyDbContext():base("Legacy")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().ToTable("Products");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
